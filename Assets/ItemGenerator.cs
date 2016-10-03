5e5766f335b8acee0b4d38e03abd107a0d70dbc0
minoru.nishida <djwanpa@gmail.com>
2016-10-04 00:38:57 +0900

    first commit

---
+++ Assets/ItemGenerator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemGenerator : MonoBehaviour {
+	//carPrefabを入れる
+	public GameObject carPrefab;
+	//coinPrefabを入れる
+	public GameObject coinPrefab;
+	//conePrefabを入れる
+	public GameObject conePrefab;
+	//スタート地点
+	private int starPos = -160;
+	//ゴール地点
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/UnityChan/Models/MyCameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyCameraController : MonoBehaviour {
+	//ユニティちゃんのオブジェクト
+	private GameObject unitychan;
+	//ユニティちゃんとのカメラの距離
+	private float difference;
+
+	// Use this for initialization
+	void Start () {
+		//ユニティちゃんのオブジェクトを取得
+		this.unitychan = GameObject.Find("unitychan");
+		//ユニティちゃんとカメラ位置（z座標）の差を求める
+		this.difference = unitychan.transform.position.z - this.transform.position.z;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//ユニティちゃんの位置いあわせてカメラ位置を移動
+		this.transform.position = new Vector3(0, this.transform.position.y, this.unitychan.transform.position.z-difference);	
+	}
+}
