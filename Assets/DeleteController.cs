829c994f6f60c8557530765fd90a63d0c8e87ce4
minoru.nishida <djwanpa@gmail.com>
2016-10-16 15:41:56 +0900

    no message

--- Assets/DeleteController.cs
+++ Assets/DeleteController.cs
@@ -6,7 +6,8 @@ public class DeleteController : MonoBehaviour {
 	
 
 	public GameObject[] coins;
-
+	public GameObject[] trafficCones;
+	public GameObject[] cars;
 	void Start () {
 
 
@@ -21,6 +22,24 @@ public class DeleteController : MonoBehaviour {
 			//Debug.Log(coin.transform.position.z);
 			if ((this.transform.position.z -1) > coin.transform.position.z) {
 				Destroy(coin);
+			}
+			 
+		}
+
+		trafficCones = GameObject.FindGameObjectsWithTag ("TrafficConeTag");
+		foreach (GameObject traffiCone in trafficCones) {
+			//Debug.Log(coin.transform.position.z);
+			if ((this.transform.position.z -1) > traffiCone.transform.position.z) {
+				Destroy(traffiCone);
+			}
+			 
+		}
+
+		cars = GameObject.FindGameObjectsWithTag ("CarTag");
+		foreach (GameObject car in cars) {
+			//Debug.Log(coin.transform.position.z);
+			if ((this.transform.position.z -1) > car.transform.position.z) {
+				Destroy(car);
 			}
 			 
 		}
