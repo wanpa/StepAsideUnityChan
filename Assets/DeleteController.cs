9f4db75ac9bedcaef192155e9698f031bcdd0f3a
minoru.nishida <djwanpa@gmail.com>
2016-10-16 15:27:55 +0900

    課題：不要になったアイテムを順次破棄しましょう

---
+++ Assets/DeleteController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeleteController : MonoBehaviour {
+
+	
+
+	public GameObject[] coins;
+
+	void Start () {
+
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		//Debug.Log (this.transform.position.z);
+		coins = GameObject.FindGameObjectsWithTag ("CoinTag");
+		foreach (GameObject coin in coins) {
+			//Debug.Log(coin.transform.position.z);
+			if ((this.transform.position.z -1) > coin.transform.position.z) {
+				Destroy(coin);
+			}
+			 
+		}
+
+	}
+
+}
